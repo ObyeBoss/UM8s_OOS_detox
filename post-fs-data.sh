de6b63a46bc19ea6d6ac4b86a78549771eb44d3f
UltraM8 <arssovetnikov@gmail.com>
2025-07-07 08:47:50 +0300

    v1.37
    update some configs
    remove ofs to fix bootloop issues with susfs

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -7,16 +7,8 @@ mf() {
     rp="${f#$sd}"; tf="$td$rp"; mount -o bind "$f" "$tf"
   done
 }
-mfo() {
-  local s="$1"; local d="$2"; local wb="/dev/overlay_work"; local up="$wb/upper${d}"; local wo="$wb/work${d}"
-  [ ! -d "$s" ] && return; [ -d "$d" ] || return; mkdir -p "$up" "$wo"; cp -a "$s/." "$up/"; mount -t overlay overlay -o lowerdir="$d",upperdir="$up",workdir="$wo" "$d"
-}
-for part in /data; do
+for part in /data /my_product /my_region /my_stock; do
   pn="${part#/}"; sd="$MODDIR/$pn"; td="/$pn"; mf "$sd" "$td"
 done
-for part2 in /my_product /my_region /my_stock; do
-  pn="${part2#/}"; s="$MODPATH/$pn"; d="/$pn"; mfo "$s" "$d"
-done
 DEST="/my_product/vendor/etc"; SRC="multimedia_display_feature_config.xml"; mount -o bind "$MODPATH"/$SRC $DEST/$SRC
-DEST2="/data/used_de/0/com.android.systemui/shared_prefs"; SRC2="oplus_media_controller_config_sp.xml"; mount -o bind "$MODPATH"/$SRC2 $DEST2/$SRC2
 echo "0" > /sys/kernel/tracing/events/camera/cam_log_debug/enable; echo "0" > /sys/kernel/tracing/events/camera/cam_log_event/enable; echo "0" > /sys/kernel/tracing/events/camera/cam_tracing_mark_write/enable
--- service.sh
+++ service.sh
@@ -14,22 +14,22 @@ resetprop -p --delete persist.vendor.ims.disableDebugLogs; resetprop -p --delete
   fi
   chown "$OWNER" "$DEST"; chmod 660 "$DEST"; chcon "$CONTEXT" "$DEST"
 )&
-sleep 40; rm -rf /data/tombstones/*; rm -rf /mnt/oplus/op2/media/log/*; cmd wifi force-country-code enabled 00; cmd wifi set-verbose-logging disabled; echo "1" > /proc/sys/net/ipv4/tcp_window_scaling; echo "900" > /proc/sys/net/ipv4/tcp_max_reordering; echo "bbr" > /proc/sys/net/ipv4/tcp_congestion_control; echo "0" > /proc/sys/net/ipv4/tcp_slow_start_after_idle; echo "0" > /proc/sys/net/ipv4/tcp_no_metrics_save
+sleep 15; rm -rf /data/tombstones/*; rm -rf /mnt/oplus/op2/media/log/*; cmd wifi force-country-code enabled 00; cmd wifi set-verbose-logging disabled; echo "1" > /proc/sys/net/ipv4/tcp_window_scaling; echo "900" > /proc/sys/net/ipv4/tcp_max_reordering; echo "bbr" > /proc/sys/net/ipv4/tcp_congestion_control; echo "0" > /proc/sys/net/ipv4/tcp_slow_start_after_idle; echo "0" > /proc/sys/net/ipv4/tcp_no_metrics_save
 (
+  sleep 10; su -c "pm disable com.google.android.googlequicksearchbox"; sleep 5; su -c "pm enable com.google.android.googlequicksearchbox"
   pm list packages | sed 's/package://' | grep -v -E '^(vendor\.qti|com\.qti)\.' | while read -r pkg; do
     su -c "pm disable $pkg/.service.ReceiverService" >/dev/null 2>&1
-  done
-  for pkg in com.oplus.olc com.oplus.logkit com.oplus.engineernetwork com.oplus.onetrace com.oplus.postmanservice com.oplus.qualityprotect com.oplus.healthservice com.oplus.crashbox com.oplus.locationproxy com.oplus.lfeh com.heytap.htms com.heytap.mcs com.heytap.pictorial com.oplus.statistics.rom com.oplus.metis com.oplus.vdc; do
-    su -c "pm disable $pkg"
-  done
-  sleep 10; su -c "pm disable com.google.android.googlequicksearchbox"; sleep 5; su -c "pm enable com.google.android.googlequicksearchbox"
-  for srv in ads.AdRequestBrokerService ads.measurement.GmpConversionTrackingBrokerService ads.social.GcmSchedulerWakeupService ads.identifier.service.AdvertisingIdNotificationService ads.identifier.service.AdvertisingIdService .ads.jams.NegotiationService ads.cache.CacheBrokerService adid.service.AdIdProviderService adsidentity.service.AdServicesExtDataStorageService analytics.AnalyticsTaskService analytics.internal.PlayLogReportingService analytics.AnalyticsReceiver analytics.service.AnalyticsService common.stats.net.contentprovider.NetworkUsageContentProvider common.stats.GmsCoreStatsService common.stats.StatsuploadService stats.PlatformStatsCollectorService stats.eastworld.EastworldService westworld.WestworldService clearcut.debug.ClearcutDebugDumpService clearcut.uploader.QosupdaterService measurement.service.MeasurementBrokerService measurement.PackageMeasurementService measurement.PackageMeasurementReceiver measurement.PackageMeasurementTaskService feedback.OfflineReportSendTaskService feedback.FeedbackAsyncService nearby.exposurenotification.WakeUpService personalsafety.service.SndDetectionService; do
-    su -c "pm disable com.google.android.gms/.$srv" >/dev/null 2>&1
   done
   for proc in tcpdump tcpdump_odm cnss_cli cnss_diag cnss-daemon; do
     if pgrep -f "$proc" > /dev/null; then
       su -c killall -q "$proc"; sleep 1; [ "$(pgrep -f "$proc")" ] && su -c killall -q -9 "$proc"
     fi
+  done
+  for pkg in com.oplus.olc com.oplus.logkit com.oplus.engineernetwork com.oplus.onetrace com.oplus.postmanservice com.oplus.qualityprotect com.oplus.healthservice com.oplus.crashbox com.oplus.locationproxy com.oplus.lfeh com.heytap.htms com.heytap.mcs com.heytap.pictorial com.oplus.statistics.rom com.oplus.metis com.oplus.vdc; do
+    su -c "pm disable $pkg"
+  done
+  for srv in ads.AdRequestBrokerService ads.measurement.GmpConversionTrackingBrokerService ads.social.GcmSchedulerWakeupService ads.identifier.service.AdvertisingIdNotificationService ads.identifier.service.AdvertisingIdService .ads.jams.NegotiationService ads.cache.CacheBrokerService adid.service.AdIdProviderService adsidentity.service.AdServicesExtDataStorageService analytics.AnalyticsTaskService analytics.internal.PlayLogReportingService analytics.AnalyticsReceiver analytics.service.AnalyticsService common.stats.net.contentprovider.NetworkUsageContentProvider common.stats.GmsCoreStatsService common.stats.StatsuploadService stats.PlatformStatsCollectorService stats.eastworld.EastworldService westworld.WestworldService clearcut.debug.ClearcutDebugDumpService clearcut.uploader.QosupdaterService measurement.service.MeasurementBrokerService measurement.PackageMeasurementService measurement.PackageMeasurementReceiver measurement.PackageMeasurementTaskService feedback.OfflineReportSendTaskService feedback.FeedbackAsyncService nearby.exposurenotification.WakeUpService personalsafety.service.SndDetectionService; do
+    su -c "pm disable com.google.android.gms/.$srv" >/dev/null 2>&1
   done
   sleep 5
   for pkg in com.google.android.gms com.google.android.gsf; do
